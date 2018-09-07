daaa104bb9069ae9150a64c89e18b0e6cdf44e47
unknown <vmirella_17@hotmail.com>
2018-09-07 12:13:13 -0500

    Creando componentes para el login y el home en react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './components/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Login} exact />
+          <Route path="/home" component={Home} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
