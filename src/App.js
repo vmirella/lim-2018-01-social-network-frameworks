79ca063caede37274cb1a5c586d54a14b44b5bad
unknown <vmirella_17@hotmail.com>
2018-09-07 12:14:36 -0500

    Creando componentes para el login y el home en react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Login} exact />
+          <Route path="/home" component={Home} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
