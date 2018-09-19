a14fb7759cff52d4862cb303f86d11d1d9ceaa05
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-09-19 15:22:41 -0500

    configuracion de firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React  from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
+import firebase from 'firebase';
+// import 'firebase/database'
 
 class App extends React.Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCrbUbq0oD49Yzk_eryDiJoseqOC6vUIcg",
+    authDomain: "pet-health-social-network.firebaseapp.com",
+    databaseURL: "https://pet-health-social-network.firebaseio.com",
+    projectId: "pet-health-social-network",
+    storageBucket: "pet-health-social-network.appspot.com",
+    messagingSenderId: "838633128523"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
